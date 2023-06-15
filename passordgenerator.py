518abcae00e27bc35942a2beba22e011033fcfb2
Yacan1337 <120426239+Yacan1337@users.noreply.github.com>
2023-06-15 06:08:46 +0200

    Add files via upload

---
+++ passordgenerator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+def generate_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+ #Usage example
+password = generate_password(12)
+print("Generated password:", password)
